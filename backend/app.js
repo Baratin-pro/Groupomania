50c6ccbf8133bd0cc2a399f6e768c2c7a90dae1a
Baratin-Pro <baratin.pro@gmail.com>
2021-07-14 09:47:36 +0200

    :sparkles: create page app.js

---
+++ backend/app.js
@@ -0,0 +1,32 @@
+"use strict";
+
+const express = require("express");
+const app = express();
+
+const helmet = require("helmet");
+app.use(helmet());
+
+const path = require("path");
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:4200");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const db = require("./models");
+db.sequelize.sync();
+
+app.use("/images", express.static(path.join(__dirname, "images")));
+
+module.exports = app;
